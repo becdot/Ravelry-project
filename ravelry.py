096f33ee90a14c628bfb311ec1c5a500c5bafc9c
Becca Liss <becca.liss@gmail.com>
2012-07-12 21:05:16 -0400

    Add python requests for login pages

--- ravelry.py
+++ ravelry.py
@@ -1,18 +1,28 @@
 import urllib2, json
+import requests
 
-f = urllib2.urlopen('http://api.ravelry.com/projects/becdot/progress.json?key=30820525c938d7eab6c933ce8dbe9c3ff1ee57b8&status=in-progress+hibernating+finished+frogged&notes=true')
-rawdata = json.load(f)
-
-user_details = {}
-for k, v in rawdata['user'].iteritems():
-    user_details[k] = v
+#f = urllib2.urlopen('http://api.ravelry.com/projects/becdot/progress.json?key=30820525c938d7eab6c933ce8dbe9c3ff1ee57b8&status=in-progress+hibernating+finished+frogged&notes=true')
+#rawdata = json.load(f)
+#
+#user_details = {}
+#for k, v in rawdata['user'].iteritems():
+#    user_details[k] = v
 #for k, v in user_details.iteritems(): print k, ':', v
-
-project_details = {}
-for project in rawdata['projects']:
-    for k, v in project.iteritems():
-        project_details[project['name']] = project       
+#
+#project_details = {}
+#for project in rawdata['projects']:
+#    for k, v in project.iteritems():
+#        project_details[project['name']] = project       
 #for k, v in project_details.iteritems(): print k, ':', v, '\n'
 
+login_url = 'https://www.ravelry.com/account/login'
+yarn_url = 'http://www.ravelry.com/yarns/library/brown-sheep-lambs-pride-bulky'
+auth = {'user[login]': 'becdot', 'user[password]': 'becca21'}
+login_response = requests.post(login_url, data=auth)
+print login_response.text
+print login_response.headers
 
-f.close()
+
+
+
+#f.close()
