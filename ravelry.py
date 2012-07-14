411ee7418adc1995ced2c0bde20383d450ae47b4
Becca Liss <becca.liss@gmail.com>
2012-07-13 22:07:50 -0400

    Creates files of the JSON and webscraped yarn data

--- ravelry.py
+++ ravelry.py
@@ -1,18 +1,18 @@
 import urllib2, json
 import requests
 
-#f = urllib2.urlopen('http://api.ravelry.com/projects/becdot/progress.json?key=30820525c938d7eab6c933ce8dbe9c3ff1ee57b8&status=in-progress+hibernating+finished+frogged&notes=true')
-#rawdata = json.load(f)
-#
-#user_details = {}
-#for k, v in rawdata['user'].iteritems():
-#    user_details[k] = v
+jsondata = urllib2.urlopen('http://api.ravelry.com/projects/becdot/progress.json?key=30820525c938d7eab6c933ce8dbe9c3ff1ee57b8&status=in-progress+hibernating+finished+frogged&notes=true')
+rawdata = json.load(jsondata)
+
+user_details = {}
+for k, v in rawdata['user'].iteritems():
+    user_details[k] = v
 #for k, v in user_details.iteritems(): print k, ':', v
-#
-#project_details = {}
-#for project in rawdata['projects']:
-#    for k, v in project.iteritems():
-#        project_details[project['name']] = project       
+
+project_details = {}
+for project in rawdata['projects']:
+    for k, v in project.iteritems():
+        project_details[project['name']] = project       
 #for k, v in project_details.iteritems(): print k, ':', v, '\n'
 
 login_url = 'https://www.ravelry.com/account/login'
@@ -21,10 +21,17 @@ auth = {'user[login]': 'becdot', 'user[password]': 'becca21'}
 session_info = requests.session()
 login_response = session_info.post(login_url, data=auth)
 yarn_response = session_info.get(yarn_url)
+yarn_response = yarn_response.text.encode('utf-8')
 
-print yarn_response.text
+userdata = open('ravelryuserdata.txt', 'w')
+projectdata = open('ravelryprojectdata.txt', 'w')
+yarnscrapedata = open('yarnscrapedata.txt', 'w')
+userdata.write('%s\n' % repr(user_details))
+projectdata.write('%s\n' % repr(project_details))
+yarnscrapedata.write('%s\n' % yarn_response)
 
 
-
-
-#f.close()
+jsondata.close()
+userdata.close()
+projectdata.close()
+yarnscrapedata.close()
